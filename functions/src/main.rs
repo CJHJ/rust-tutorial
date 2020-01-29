aeb25fe9904078b9be8eab08cfd5251eb3a8de61
CJHJ <corronade@gmail.com>
2020-01-29 15:31:40 +0900

    Update function code

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,6 +3,16 @@ fn main() {
 }
 
 fn another_function(x: i32, y: i32) {
+    let z = {
+        let x = 3;
+        x + 1
+    };
+
     println!("The value is {}", x);
     println!("The value of y is {}", y);
+    println!("The value of z is {}, five is {}", z, five());
+}
+
+fn five() -> i32 {
+    5
 }
