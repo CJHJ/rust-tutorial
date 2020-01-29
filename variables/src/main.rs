6f470f40f82b0c3b4b0a4772f5ecffb6eb3a0474
CJHJ <corronade@gmail.com>
2020-01-29 15:16:32 +0900

    Commit projects until functions

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+
+    // Array
+    let a = [1, 2, 3, 4, 5];
+    let index = 2;
+
+    let element = a[index];
+
+    println!("The value of element is: {}", element);
+}
